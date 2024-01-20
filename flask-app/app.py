1f9c94a54e3036f45a6515ebe4564249ed46d8bc
michaeljgoerlitz <mjgoerlitz@icloud.com>
2024-01-20 01:05:58 -0500

    created flask version of todo

---
+++ flask-app/app.py
@@ -0,0 +1,111 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+# Initialize the database outside of any function
+db = SQLAlchemy()
+
+# Define your Todo model here
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
+# Application factory function
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    # Define your routes here
+    @app.get("/")
+    def home():
+        todo_list = db.session.query(Todo).all()
+        return render_template("base.html", todo_list=todo_list)
+
+    @app.post("/add")
+    def add():
+        title = request.form.get("title")
+        new_todo = Todo(title=title, complete=False)
+        db.session.add(new_todo)
+        db.session.commit()
+        return redirect(url_for("home"))
+
+    @app.get("/update/<int:todo_id>")
+    def update(todo_id):
+        todo = db.session.query(Todo).filter(Todo.id == todo_id).first()
+        todo.complete = not todo.complete
+        db.session.commit()
+        return redirect(url_for("home"))
+
+    @app.get("/delete/<int:todo_id>")
+    def delete(todo_id):
+        todo = db.session.query(Todo).filter(Todo.id == todo_id).first()
+        db.session.delete(todo)
+        db.session.commit()
+        return redirect(url_for("home"))
+
+    return app
+
+# Create the app using the factory
+app = create_app()
+
+# Ensure this is at the bottom of your script
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+
+# from flask import Flask, render_template, request, redirect, url_for
+# from flask_sqlalchemy import SQLAlchemy
+
+
+# app = Flask(__name__)
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
+
+
+# class Todo(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(100))
+#     complete = db.Column(db.Boolean)
+
+# db.create_all()
+
+# @app.get("/")
+# def home():
+#     todo_list = db.session.query(Todo).all()
+#     return render_template("base.html", todo_list=todo_list)
+
+
+# @app.post("/add")
+# def add():
+#     title = request.form.get("title")
+#     new_todo = Todo(title=title, complete=False)
+#     db.session.add(new_todo)
+#     db.session.commit()
+#     return redirect(url_for("home"))
+
+
+# @app.get("/update/<int:todo_id>")
+# def update(todo_id):
+#     todo = db.session.query(Todo).filter(Todo.id == todo_id).first()
+#     todo.complete = not todo.complete
+#     db.session.commit()
+#     return redirect(url_for("home"))
+
+# @app.get("/delete/<int:todo_id>")
+# def delete(todo_id):
+#     todo = db.session.query(Todo).filter(Todo.id == todo_id).first()
+#     db.session.delete(todo)
+#     db.session.commit()
+#     return redirect(url_for("home"))
